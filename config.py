adaaa5dfd266ea2326cb50484c019d2b1b513b75
1443213244 <1443213244@qq.com>
2020-12-31 14:32:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ private_ip = ''       #Please do not fill in if public network forwards
 dbhost = '127.0.0.1'
 dbuser = 'root'
 dbport = '3306'
-db = 'relays'
+dbname = 'relays'
 dbpassword = '123456'
 mode = 'master' #mode in (master/relay)
