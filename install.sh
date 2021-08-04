5a8f692f29834dd64fb8ade15da3dfbce3402fbb
1443213244 <1443213244@qq.com>
2021-08-04 20:48:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo "首次使用正在安装运行环境"
 mkdir ./data
 systemctl stop firewalld.service
 systemctl disable firewalld.service
+yum install epel-release -y
 yum install python-devel supervisor iptables-services -y
 systemctl enable iptables.service
 if [ $(sysctl -n net.ipv4.ip_forward) == 0 ]; then
